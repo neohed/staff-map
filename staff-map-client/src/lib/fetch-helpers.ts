d2054ff8e56c4670d2955f17e1eaacf22fd59064
neohed <35173474+neohed@users.noreply.github.com>
2023-03-17 15:27:36 +0000

    Update fetch-helpers.ts

--- staff-map-client/src/lib/fetch-helpers.ts
+++ staff-map-client/src/lib/fetch-helpers.ts
@@ -1,18 +1,20 @@
-function getFetchHeaders(token, setContentType) {
+function getFetchHeaders({token, setContentType, contentType}: {token: string, setContentType: boolean, contentType: string}) {
     const headers = new Headers();
 
-    if (setContentType) {
-        headers.append('Content-Type', 'application/json');
+    if (contentType) {
+        headers.append('Content-Type', contentType)
+    } else if (setContentType) {
+        headers.append('Content-Type', 'application/json')
     }
 
     if (token) {
-        headers.append('Authorization', token);
+        headers.append('Authorization', token)
     }
 
     return headers
 }
 
-function createFormDataObject(formData) {
+function createFormDataObject(formData: object) {
     const data = new FormData();
 
     Object.entries(formData).forEach(([key, value]) => {
@@ -22,45 +24,59 @@ function createFormDataObject(formData) {
     return data
 }
 
-const getUrl = url => new URL(url, process.env.REACT_APP_API).toString();
+const getUrl = (url: string) => new URL(url, process.env.REACT_APP_API).toString();
 
-const getQuerystring = properties => '?' + Object.entries(properties).map(([key, value]) => `${key}=${encodeURI(value)}`).join('&');
+const getQuerystring = (properties: object) => '?' + Object.entries(properties).map(([key, value]) => `${key}=${encodeURI(value)}`).join('&');
 
-function getFetchOptions(postData, options = {}) {
+type FetchOptionParams = {
+    token: string;
+    method: string;
+    contentType: string;
+    isPost?: boolean;
+    stringify?: boolean;
+    setContentType?: boolean;
+}
+
+function getFetchOptions(postData: object, options: FetchOptionParams): RequestInit {
     const hasPostData = !!postData;
     const isFormData = hasPostData && postData instanceof FormData;
 
     const {
         token,
+        method,
         contentType,
         isPost = false,
         stringify = !isFormData, // Don't stringify FormData objects.
         setContentType = !isFormData, // FormData contentType gets auto detected.
     } = options;
 
-    const fetchOptions = {
-        method: (isPost || hasPostData) ? 'POST' : 'GET',
-        headers: getFetchHeaders(token, setContentType, contentType)
+    const fetchOptions: RequestInit = {
+        method: method ?? ((isPost || hasPostData) ? 'POST' : 'GET'),
+        headers: getFetchHeaders({token, setContentType, contentType}),
     };
 
     if (postData) {
         fetchOptions.body = stringify
-            ? JSON.stringify(postData)
-            : postData;
+            ? JSON.stringify(postData) as BodyInit
+            : postData as BodyInit;
     }
 
     return fetchOptions
 }
 
-async function doFetch(url, postData, options, success, fail) {
-    const response = await fetch(getUrl(url), getFetchOptions(postData, options));
+async function doFetch(url: string, postData: object, options: FetchOptionParams) {
+    const response = await fetch(
+        getUrl(url),
+        getFetchOptions(postData, options)
+    );
 
-    if (!response.ok) {
-        return fail()
+    if (response.ok) {
+        return await response.json();
+    } else {
+        const errorMessage = await response.text();
+
+        throw new Error(errorMessage)
     }
-
-    const data = await response.json();
-    return success(data)
 }
 
 export {
