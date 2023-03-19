4a7139bd869a57f6898e53bb867469ae1981f3aa
neohed <35173474+neohed@users.noreply.github.com>
2023-03-19 20:24:37 +0000

    google types

--- staff-map-client/src/App.tsx
+++ staff-map-client/src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import SimpleMap from "./components/SimpleMap";
+//import SimpleMap from "./components/SimpleMap";
+import ShapesMap from "./components/ShapesMap";
+import type {MapStyles} from "./components/types";
 import './App.css';
+
+const mapProps: MapStyles = {
+    container: {
+        width: '1200px',
+        height: '800px'
+    }
+}
 
 function App() {
   return (
     <div className="App">
-        <SimpleMap />
+        <ShapesMap styles={mapProps} />
     </div>
   );
 }
