94d6dfdb87c7dc3dcf661756affcf4aaba7c92a8
dave <35173474+neohed@users.noreply.github.com>
2023-03-20 16:45:13 +0000

    Update start-checks.js

--- staff-map-client/_scripts/start-checks.js
+++ staff-map-client/_scripts/start-checks.js
@@ -3,7 +3,7 @@ require("dotenv").config({ path: './.env.local' });
 const log = {
     _: (code, msg) => console.log(`\x1b[${code}m${msg}\x1b[0m`),
     info: msg => log._(92, msg),
-    warning: msg => log._(93, msg),
+    warn: msg => log._(93, msg),
     error: msg => log._(91, msg)
 }
 
@@ -12,8 +12,8 @@ const gmailApiKey = process.env[gmailApiKeyName];
 
 if (gmailApiKey === undefined || gmailApiKey === '') {
     log.error(`Error! Key: "${gmailApiKeyName}" in environment file: ".env.local" not found.`);
-    log.error('Please ensure both the env file and API key exist.');
-    log.warning('For help creating a Google maps API key, see section "Configuring Google Maps API Key" in "README.md".\n\n');
+    log.warn('Please ensure both the env file and API key exist.');
+    log.warn('For help creating a Google maps API key, see section "Configuring Google Maps API Key" in "README.md".\n\n');
 
     process.exit(1) // error
 } else {
