34351c33502a2766be547adda44ed04dc4a91888
dave <35173474+neohed@users.noreply.github.com>
2023-03-22 15:45:56 +0000

    Update db.ts

--- staff-map-server/src/model/db.ts
+++ staff-map-server/src/model/db.ts
@@ -1,14 +1,14 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
-    prisma = new PrismaClient()
+    prisma = new PrismaClient();
 } else {
-    const {__db__} = global;
+    const { __db__ } = global as any;
 
     if (__db__) {
-        prisma = __db__
+        prisma = __db__;
     } else {
         prisma = new PrismaClient({
             log: [
@@ -22,15 +22,15 @@ if (process.env.NODE_ENV === "production") {
             ],
         });
 
-        prisma.$on("query", ({query, duration}) => {
+        prisma.$on("query", ({ query, duration }) => {
             console.log(`\x1b[36mprisma:query\x1b[0m ${query}`);
-            console.log(`Took: ${duration}ms`)
+            console.log(`Took: ${duration}ms`);
         });
 
-        global.__db__ = prisma
+        (global as any).__db__ = prisma;
     }
 
     prisma.$connect();
 }
 
-export default prisma
+export default prisma;
