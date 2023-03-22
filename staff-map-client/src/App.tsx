e884cddeda5fc871f6b38e8f1bef35b4adba0f44
dave <35173474+neohed@users.noreply.github.com>
2023-03-22 16:01:05 +0000

    Basic auth working.

--- staff-map-client/src/App.tsx
+++ staff-map-client/src/App.tsx
@@ -9,11 +9,11 @@ import SearchMap from "./components/SearchMap";
  */
 import SimpleMap from "./components/SimpleMap";
 import type {MapStyles} from "./components/types";
-import './App.css';
 import {Login} from "./modules/account";
-//import SetValueContext, {getContextValue} from "./lib/SetValueContext";
 import {Routes, Route, Link} from 'react-router-dom'
 import Home from "./components/Home";
+import {AuthRoute} from "./modules/account"
+import './App.css';
 
 const mapProps: MapStyles = {
     container: {
@@ -23,93 +23,27 @@ const mapProps: MapStyles = {
 }
 
 function App() {
-    /*
-    const context = useContext(SetValueContext)
-    useEffect(() => {
-        context.setValue('test', 1)
-        context.setValue('a', 12)
-    }, [])
-    console.log(getContextValue(context, 'AUTH'));
-    console.log(getContextValue(context, 'a'));
-     */
-
-  return (
-    <div className="App">
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/map">Map</Link></li>
-            </ul>
-        </nav>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/map" element={<SimpleMap styles={mapProps} />} />
-            {
-                /* <Route path="/map/:id" element={<SimpleMap styles={mapProps} />} />
-                 * const {id} = useParams()
-                 */
-            }
-            {
-                /*
-                <AdminLayout /> needs to contain an <Outlet /> for the nested route content to appear in!
-                Can do: <Outlet context={{ message: 'Hello nested routes!'}} />
-                then: const {message} = useOutletContext(); in nested route elements;
-
-                Can copy a route somewhere else, e.g., a sidebar or nav-bar, to get additional content for a route.
-
-                Use a .isActive {} CSS class to style active links!
-                or
-                <NavLink style={({ isActive }) => isActive ? {'color: red'} : {} }>
-                    {
-                        ({ isActive }) => ({
-                            // Render Props!
-                        })
-                    }
-                </NavLink>
-
-                To navigate (redirect) return <Navigate to="/" />
-                or
-                const navigate = useNavigate()
-                navigate("/", { optional_state: 1 })
-                or
-                navigate(-1) // like back button
-
-                Search params:
-                const [searchParams, setSearchParams] = useSearchParams({n: 3})
-                const num = searchParams.get('n');
-                setSearchParams({ n: 111 })
-
-                <Link ... state="Fred"
-                =>
-                const location = useLocation();
-                location => { hash:"#test", key:"n8Xfg", pathname:"/", search:"?n=10", state:"Fred" }
-
-                Auth!
-                <Route path="/login" element={<LoginPage />} />
-                  <Route
-                    path="/protected"
-                    element={
-                      <RequireAuth>
-                        <ProtectedPage />
-                      </RequireAuth>
-                    }
-                  />
-                </Route>
-
-                
-
-                <Route path="/account" element={<AdminLayout />}>
-                    <Route index element={<Account />} />
-                    <Route path="login" element={<Login />} />
-                    <Route path="register" element={<Register />} />
-                </Route>
-                 */
-            }
-            <Route path="*" element={<div>404</div>} />
-        </Routes>
-        <Login />
-    </div>
-  );
+    return (
+        <div className="App">
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/map">Map</Link></li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/map" element={
+                    <AuthRoute>
+                        <SimpleMap styles={mapProps}/>
+                    </AuthRoute>
+                }/>
+                <Route path="*" element={<div>404</div>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- staff-map-client/src/modules/account/index.ts
+++ staff-map-client/src/modules/account/index.ts
@@ -1,15 +1,7 @@
 import Login from "./components/Login";
 import AuthRoute from "./components/AuthRoute";
-/*
-import AuthButton from "./components/AuthButton";
-import authProvider from "./components/authProvider";
- */
 
 export {
     Login,
     AuthRoute,
-    /*
-    AuthButton,
-    authProvider,
-     */
 }
