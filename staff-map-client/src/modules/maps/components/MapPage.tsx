74534731661fd6798e4b579bfe17abfe120f91cd
dave <35173474+neohed@users.noreply.github.com>
2023-03-23 15:22:53 +0000

    Hook seems unreliable for loading scripts!

--- staff-map-client/src/modules/maps/components/MapPage.tsx
+++ staff-map-client/src/modules/maps/components/MapPage.tsx
@@ -1,19 +1,38 @@
 import React, {useState} from "react";
+import {LoadScript} from '@react-google-maps/api'
+import envVars from "../../../lib/env-vars";
+import {libraries} from "./map-options";
 import Layout from './Layout';
 import Map from './Map';
+
+const onLoad = () => console.log('script loaded')
+const onError = (err: Error) => console.log('onError: ', err)
+const Loading = <div>Loading...</div>;
 
 const MapPage = () => {
     const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
     return (
-        <Layout
-            isReady={isLoaded}
-            main={
-                <Map
-                    setReady={setIsLoaded}
-                />
-            }
-        />
+        <LoadScript
+            id="staff-maps-id"
+            googleMapsApiKey={envVars.GOOGLE_MAPS_API_KEY}
+            region='EN'
+            version='weekly'
+            onLoad={onLoad}
+            onError={onError}
+            loadingElement={Loading}
+            libraries={libraries}
+            preventGoogleFontsLoading={false}
+        >
+            <Layout
+                isReady={isLoaded}
+                main={
+                    <Map
+                        setReady={setIsLoaded}
+                    />
+                }
+            />
+        </LoadScript>
     )
 }
 
--- staff-map-client/src/modules/maps/components/map-options.ts
+++ staff-map-client/src/modules/maps/components/map-options.ts
@@ -1,53 +1,58 @@
+import {Libraries} from "@react-google-maps/api/dist/utils/make-load-script-url";
+
 const center: google.maps.LatLngLiteral = {
-  lat: 51.50052933987512,
-  lng: -0.12615771706025705
+    lat: 51.50052933987512,
+    lng: -0.12615771706025705
 }
 
 const mapOptions: google.maps.MapOptions = {
-  // mapId: '33c9314ebf331b36',
-  disableDefaultUI: true,
-  clickableIcons: false,
-  streetViewControl: false,
+    // mapId: '33c9314ebf331b36',
+    disableDefaultUI: true,
+    clickableIcons: false,
+    streetViewControl: false,
 }
 
 const defaultOptions = {
-  strokeOpacity: 0.5,
-  strokeWeight: 2,
-  clickable: false,
-  draggable: false,
-  editable: false,
-  visible: true,
+    strokeOpacity: 0.5,
+    strokeWeight: 2,
+    clickable: false,
+    draggable: false,
+    editable: false,
+    visible: true,
 }
 
 const closeOptions = {
-  ...defaultOptions,
-  zIndex: 3,
-  fillOpacity: 0.05,
-  strokeColor: "#8BC34A",
-  fillColor: "#8BC34A",
+    ...defaultOptions,
+    zIndex: 3,
+    fillOpacity: 0.05,
+    strokeColor: "#8BC34A",
+    fillColor: "#8BC34A",
 }
 
 const middleOptions = {
-  ...defaultOptions,
-  zIndex: 2,
-  fillOpacity: 0.05,
-  strokeColor: "#FBC02D",
-  fillColor: "#FBC02D",
+    ...defaultOptions,
+    zIndex: 2,
+    fillOpacity: 0.05,
+    strokeColor: "#FBC02D",
+    fillColor: "#FBC02D",
 }
 
 const farOptions = {
-  ...defaultOptions,
-  zIndex: 1,
-  fillOpacity: 0.05,
-  strokeColor: "#FF5252",
-  fillColor: "#FF5252",
+    ...defaultOptions,
+    zIndex: 1,
+    fillOpacity: 0.05,
+    strokeColor: "#FF5252",
+    fillColor: "#FF5252",
 }
 
+const libraries: Libraries = ['drawing', 'visualization', 'places'];
+
 export {
-  center,
-  mapOptions,
-  defaultOptions,
-  closeOptions,
-  middleOptions,
-  farOptions,
+    center,
+    mapOptions,
+    defaultOptions,
+    closeOptions,
+    middleOptions,
+    farOptions,
+    libraries,
 }
