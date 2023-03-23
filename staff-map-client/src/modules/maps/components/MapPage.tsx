9b394a49094246bfc9d88974ffe4c6ca59efe2d1
dave <35173474+neohed@users.noreply.github.com>
2023-03-23 15:24:53 +0000

    Tidy

--- staff-map-client/src/modules/maps/components/MapPage.tsx
+++ staff-map-client/src/modules/maps/components/MapPage.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import {LoadScript} from '@react-google-maps/api'
 import envVars from "../../../lib/env-vars";
 import {libraries} from "./map-options";
@@ -10,7 +10,7 @@ const onError = (err: Error) => console.log('onError: ', err)
 const Loading = <div>Loading...</div>;
 
 const MapPage = () => {
-    const [isLoaded, setIsLoaded] = useState<boolean>(false);
+    //const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
     return (
         <LoadScript
@@ -25,10 +25,8 @@ const MapPage = () => {
             preventGoogleFontsLoading={false}
         >
             <Layout
-                isReady={isLoaded}
                 main={
                     <Map
-                        setReady={setIsLoaded}
                     />
                 }
             />
