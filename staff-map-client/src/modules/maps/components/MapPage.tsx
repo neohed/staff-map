c58895e60e2c6eca72bf684f199f0eadd11154b4
dave <35173474+neohed@users.noreply.github.com>
2023-03-23 10:33:33 +0000

    Use CSS

--- staff-map-client/src/modules/maps/components/MapPage.tsx
+++ staff-map-client/src/modules/maps/components/MapPage.tsx
@@ -1,20 +1,13 @@
 import React from "react";
-import type { MapStyles } from './types';
 import Layout from './Layout';
 import Map from './Map';
-
-const mapProps: MapStyles = {
-    container: {
-        width: '100%',
-        height: 'calc(100vh - 62px)'
-    }
-}
 
 const MapPage = () => {
     return (
         <Layout
             main={
-                <Map styles={mapProps} />
+                <Map
+                />
             }
         />
     )
