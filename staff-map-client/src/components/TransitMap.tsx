3d6baffe48a3a60a8525709368200a3b7b71f359
dave <35173474+neohed@users.noreply.github.com>
2023-03-24 15:51:27 +0000

    Remove maps

--- staff-map-client/src/components/TransitMap.tsx
+++ staff-map-client/src/components/TransitMap.tsx
@@ -39,6 +39,22 @@ function ExampleTransit({ styles }: MapProps): JSX.Element {
         return <MapLoading />
     }
 
+    /*
+    const [map, setMap] = React.useState(null)
+
+    const onLoad = React.useCallback(function callback(map) {
+        // This is just an example of getting and using the map instance!!! don't just blindly copy!
+        const bounds = new window.google.maps.LatLngBounds(center);
+        map.fitBounds(bounds);
+
+        setMap(map)
+    }, [])
+
+    const onUnmount = React.useCallback(function callback(map) {
+        setMap(null)
+    }, [])
+     */
+
     return (
         <div className='map'>
             <div className='map-container'>
@@ -50,6 +66,11 @@ function ExampleTransit({ styles }: MapProps): JSX.Element {
                     onClick={onClick}
                     onLoad={onMapLoad}
                 >
+                { /*
+                   * onUnmount={onUnmount}
+                   * Child components, such as markers, info windows, etc.
+                   *
+                   */ }
                     <TransitLayer onLoad={onTransitLayerLoad} />
                 </GoogleMap>
             </div>
